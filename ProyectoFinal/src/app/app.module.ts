2cee23d58c692569e45167055c1633cebc46d3ce
jlcm <jlcarrion90@gmail.com>
2023-02-07 00:46:48 -0500

    Estructura

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { ToolbarComponent } from './layout/components/toolbar/toolbar.component';
+
+import { ContentComponent } from './layout/components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
--- ProyectoFinal/src/app/material.module.ts
+++ ProyectoFinal/src/app/material.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [],
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   exports:[
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
